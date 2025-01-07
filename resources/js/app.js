d4551b7d0b793e596653539a7300f5ba51c74961
Fikri Mastor <fikrimastor@gmail.com>
2025-01-08 05:24:06 +0800

    feat: debugging app.js and ziggy

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,7 +12,7 @@ import Layout from './Layouts/Public.svelte';
 
 createInertiaApp({
     resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true })
+        const pages = import.meta.glob('./Pages/**/*.svelte', { eager: false })
         return pages[`./Pages/${name}.svelte`]
     },
     setup({ el, App, props }) {
@@ -22,4 +22,4 @@ createInertiaApp({
         color: "#4B5563",
         showSpinner: true,
     },
-}).then(r => {})
+})
